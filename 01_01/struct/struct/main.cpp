88043b4ea03e9dd4b9c79634c8cbdc72314bceb9
iphantom <cg090201@163.com>
2014-10-26 23:38:15 +0800

    wu

---
+++ 01_01/struct/struct/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct queue
+{
+	int data[100];//队列主体  用来存储内容
+	int head;//队首
+	int tail;//队尾
+
+};
+
+int main()
+{
+	struct queue q;
+	int i;
+	//初始化队列
+	q.head=1;
+	q.tail=1;
+	for (i=1;i<=9;i++)
+	{
+		//依次向队列插入9个数字
+		scanf("%d",&q.data[q.tail]);
+		q.tail++;
+	}
+
+	while(q.head<q.tail) //队列不为空的时候执行循环
+	｛
+		//打印队首并将队首出队
+		printf("%d ",q.data[q.head]);
+		q.head++;
+
+		//将新队首的数添加到队尾
+		q.data[q.tail]=q.data[q.head];
+		q.tail++
+
+		//再将队首出队
+		q.head++;
+	｝
+		getchar();getchar();
+	return 0;
+}
